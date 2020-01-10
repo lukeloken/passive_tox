3f57e10214d95020ba35ce988b7922e322c5782f
Luke Loken <lukeloken@gmail.com>
2020-01-10 15:42:43 -0600

    Script to start analyzing passive pesticide using toxEval. Output a few figures from the tutorial

---
+++ R/ExploreToxEval.R
@@ -0,0 +1,51 @@
+
+
+
+tox_list<- create_toxEval(file_in(file.path(path_to_data, "Data/PassiveForToxEval.xlsx")))
+
+ACClong <- get_ACC(tox_list$chem_info$CAS)
+ACClong <- remove_flags(ACClong)
+
+cleaned_ep <- clean_endPoint_info(end_point_info)
+filtered_ep <- filter_groups(cleaned_ep, 
+                             groupCol = 'intended_target_family',
+                             remove_groups = c('Background Measurement','Undefined'))
+
+chemicalSummary <- get_chemical_summary(tox_list, 
+                                        ACClong, 
+                                        filtered_ep)
+
+chemicalSummary
+
+chem_class_plot <- plot_tox_boxplots(chemicalSummary,
+                                     category = 'Chemical Class')
+chem_class_plot
+ggsave(file_out(file.path(path_to_data, "Figures/PassiveBoxplot_chem_class.png")), height=5, width=5)
+
+plot_stacks <- plot_tox_stacks(chemicalSummary, 
+                               tox_list$chem_site, 
+                               category = "Chemical Class")
+plot_stacks
+ggsave(file_out(file.path(path_to_data, "Figures/PassiveStacks_chem_class.png")), height=5, width=7)
+
+
+plot_heat <- plot_tox_heatmap(chemicalSummary, 
+                              tox_list$chem_site, 
+                              category = "Chemical Class",
+                              font_size = 7)
+plot_heat
+
+make_tox_map(chemicalSummary, 
+             chem_site = tox_list$chem_site,
+             category = 'Chemical Class',
+             mean_logic = FALSE)
+
+
+
+bio_plot <- plot_tox_boxplots(chemicalSummary, 
+                              category = 'Biological',
+                              mean_logic = FALSE,
+                              hit_threshold = NA,
+                              title = 'Summing EARs for chemicals within a grouping of a sample, taking the max of each site',
+                              plot_ND = TRUE)
+bio_plot
