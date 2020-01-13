822eeb1f05b4d612d5e554d9a2ed2f5859f886be
Luke Loken <lukeloken@gmail.com>
2020-01-13 13:22:56 -0600

    playing with another cas file. Need to combine and use all CAS information.

--- passive_data_setup_2016.R
+++ passive_data_setup_2016.R
@@ -22,11 +22,28 @@ source("R/analyze/data_reader_2016_Loken.R")
 source("R/analyze/get_chem_info.R")
 # source("R/analyze/create_tox_file.R")
 
-
+#Load chemical lists
 cas_df <- read.csv(file_in(file.path(path_to_data, 'Data/Pesticides_2016_monitoring_list_GLRI_USGS.csv'))) %>%
   rename(chnm = Chemical.Name) %>%
   mutate(chnm = tolower(chnm))
 
+cas_df_new <- read_excel(file_in(file.path(path_to_data, 'Data/pesticide CAS numbers.xlsx'))) %>%
+  rename(chnm = `Parameter Name`,
+         CAS = `CAS Number`, 
+         ParaCode = `Parameter Code`) %>%
+  mutate(chnm = tolower(chnm))
+
+#Currently not usin this one. 
+#Find single list of chemicals to use
+chem_info = read.csv(file_in(file.path(path_to_data, "Data/chemical_classes.csv")), stringsAsFactors = FALSE)
+chem_info = rename(chem_info, Chemical = Chemical.Name)
+
+
+setdiff(cas_df$chnm, cas_df_new$chnm)
+setdiff( cas_df_new$chnm, cas_df$chnm)
+
+
+#Load pesticide data
 WW_2016 = generic_file_opener(file_in(file.path(path_to_data, "Data/GLRI 2016 POCIS pesticide data report.xlsx")), cas_df, 
                               sheet = "select data",
                               n_max=60,
@@ -47,21 +64,21 @@ WW_2016_forToxEval <- WW_2016 %>%
   group_by(SiteID,  chnm, CAS) %>%
   summarize(Value = mean(Value), `Sample Date` = min(`Sample Date`))
 
-chem_info = read.csv(file_in(file.path(path_to_data, "Data/chemical_classes.csv")), stringsAsFactors = FALSE)
-chem_info = rename(chem_info, Chemical = Chemical.Name)
 
 # AOP_crosswalk = read.csv(file_in(file.path(path_to_data, "Data/AOP_crosswalk.csv")))
 
 sites_2016 = readxl::read_excel(file_in(file.path(path_to_data, "Data/GLRI 2016 POCIS pesticide data report.xlsx")), sheet = "Site List", skip = 2) %>%
   rename(site_no = `USGS Station ID`) %>%
-  mutate(`Short Name` = substr(`Site Name`, start = 1, stop = 8)) %>%
+  # mutate(`Short Name` = substr(`Site Name`, start = 1, stop = 8)) %>%
   select(`Short Name`, `site_no`)
   
 locations <- readNWISsite(sites_2016$site_no)[c("site_no", "dec_lat_va", "dec_long_va")]
 names(locations) <- c("site_no", 'dec_lat', 'dec_long')
 
 sites_2016<-left_join(sites_2016, locations) %>%
-  rename(SiteID = site_no)
+  rename(SiteID = site_no) %>%
+  filter(SiteID %in% unique(WW_2016_forToxEval$SiteID)) %>%
+  distinct()
 
 
 exclude = get_exclude(file.path(path_to_data, "Data/exclude.csv"))
