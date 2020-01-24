d4824b6b0bcb53c7fb8572834fe1a45b1fed1390
Luke Loken <lukeloken@gmail.com>
2020-01-24 14:45:09 -0600

    add maps and scatterplots to workflow

--- PassiveData2016_Workflow.R
+++ PassiveData2016_Workflow.R
@@ -8,6 +8,8 @@
 
 path_to_data <- c('C:/Users/lloken/OneDrive - DOI/GLRI_Pesticides')
 
+library(gridExtra)
+
 #Load passive sampler data and generate toxEval file
 #Saves excel file for both select and all data
 source('passive_data_setup_2016.R')
@@ -21,3 +23,5 @@ source('Plot_PassiveTox_2016.R')
 
 #Make map figure of EAR, number of chemicals detected with watersheds colored by % ag
 source('R/map_sites_passive_2016.R')
+
+source('R/Plot_scatterplots_perAg_chemicals.R')
