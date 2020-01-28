f3c033afd1f0b7c5c7f565fa0a09c17e9aec86b8
Luke Loken <lukeloken@gmail.com>
2020-01-28 15:20:54 -0600

    code to extract the legend element of a figure

---
+++ R/functions/g_legend.R
@@ -0,0 +1,8 @@
+
+
+# function to extract legent from gg object
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
