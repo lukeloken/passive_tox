c68e326b9c687078d059cc2a3adc1fe7618b3354
Luke Loken <lukeloken@gmail.com>
2020-02-03 16:21:36 -0600

    Code is a duplicate of passive plots. Still working on it.

--- Plot_SurfaceTox_2016.R
+++ Plot_SurfaceTox_2016.R
@@ -1,79 +1,96 @@
 
 
-#Calculate chemical summary for passive pesticide data
-# tox_list<- create_toxEval(file_in(file.path(path_to_data, "Data/PassiveForToxEval.xlsx")))
+#Calculate chemical summary for surface water pesticide data
+# tox_list_surface <- create_toxEval(file_in(file.path(path_to_data, "ToxEvalFiles/WQ_pesticides.xlsx")))
 
 #R objects
-chemicalSummary #Select data from POCIS 
-chemicalSummary_allpocis #all data from POCIS
-chemicalSummary_bench # Select data using custom benchmarks
+chemicalSummary_surface #Sam's data from surface
+chemicalSummary_bench_surface # Sam's surface data using custom benchmarks
 
 #Check out the standard boxplots
-plot_tox_boxplots(chemicalSummary_bench, 
+plot_tox_boxplots(chemicalSummary_bench_surface, 
                   category = "Chemical", 
                   sum_logic = FALSE,
                   x_label = "Toxicity Quotient")
 
-plot_tox_boxplots(chemicalSummary, 
+plot_tox_boxplots(chemicalSummary_surface, 
                   category = "Chemical", 
                   sum_logic = FALSE)
 
-
+# date_filter <-as.Date(c("2016-06-01", "2016-07-21"))
 
 # create tables by chemical and by sites
-# these are detections above the POCIS minimum detection
-# allpocis data only useful for presence/absence
-chem_freq_allpocis<-chemicalSummary_allpocis %>%
-  filter(EAR > 0) %>%
-  group_by(chnm, site) %>%
-  summarize(EAR_mean = mean(EAR, na.rm=T)) %>%
-  tally(name = "Detected")
+# these are detections above the minimum detection
+# some data do not have benchmarks so first identify if they were detected
 
-chemicalSummary2 <- chemicalSummary %>%
+
+
+chemicalSummary2_surface <- chemicalSummary_surface %>%
   filter(EAR>0) %>%
-  dplyr::group_by(site, chnm) %>% 
-  summarize(EAR = max(EAR, na.rm=T))
+  filter(CAS %in% unique(tox_list_allpocis$chem_data$CAS)) %>%
+  filter(site %in% unique(tox_list_allpocis$chem_site$SiteID)) %>%
+  dplyr::group_by(site, chnm, date) %>% 
+  summarize(EAR = max(EAR, na.rm=T)) %>%
+  dplyr::group_by(site, chnm) %>%
+  summarize(EAR=mean(EAR, na.rm=T))
 
-chem_freq_EAR0.001<-chemicalSummary2 %>%
+chem_freq_EAR0.001<-chemicalSummary2_surface %>%
   group_by(chnm, site) %>%
   filter(EAR >= 0.001) %>%
   summarize(EAR_max = max(EAR, na.rm=T)) %>%
   tally(name = "AboveEAR0.001")
 
-chem_freq_EAR0.0001<-chemicalSummary2 %>%
+chem_freq_EAR0.0001<-chemicalSummary2_surface %>%
   group_by(chnm, site) %>%
   filter(EAR < 0.001 & EAR >= 0.0001) %>%
   summarize(EAR_max = max(EAR, na.rm=T)) %>%
   tally(name = "AboveEAR0.0001")
 
+chem_freq_Detected<-chemicalSummary2_surface %>%
+  group_by(chnm, site) %>%
+  filter(EAR < 0.0001) %>%
+  summarize(EAR_max = max(EAR, na.rm=T)) %>%
+  tally(name = "Detected")
+
+
 chem_detection <- full_join(chem_freq_EAR0.001, chem_freq_EAR0.0001) %>%
-  full_join(chem_freq_allpocis) %>%
-  left_join(unique(chemicalSummary_allpocis[c("chnm", "Class")])) %>%
+  full_join(chem_freq_Detected) %>%
+  left_join(unique(chemicalSummary_surface[c("chnm", "Class")])) %>%
   rowwise() %>%
-  mutate(EAR_total = sum(AboveEAR0.001, AboveEAR0.0001, na.rm=T),
-         OnlyDetected = Detected - EAR_total,
-         chnm = as.character(chnm)) %>%
-  select(-Detected, -EAR_total )
+  mutate(chnm = as.character(chnm)) 
 
 
-chem_detection$chnm[which(chem_detection$Class %in% c("Deg - Fungicide", "Deg - Herbicide"))] <- 
-  paste0(chem_detection$chnm[which(chem_detection$Class %in% c("Deg - Fungicide", "Deg - Herbicide"))], "*")
+chem_detection$chnm[which(chem_detection$Class %in% c("Deg - Fungicide", "Deg - Herbicide", "Deg - Insecticide"))] <- 
+  paste0(chem_detection$chnm[which(chem_detection$Class %in% c("Deg - Fungicide", "Deg - Herbicide", "Deg - Insecticide"))], "*")
 chem_detection$Class <- gsub("Deg - ", "", chem_detection$Class)
 
 chem_detection <- chem_detection %>%
   mutate(Class = factor(Class, c('Herbicide', 'Fungicide', 'Insecticide'))) %>%
-  arrange(Class, desc(AboveEAR0.001), desc(AboveEAR0.0001), desc(OnlyDetected)) %>%
+  arrange(Class, desc(AboveEAR0.001), desc(AboveEAR0.0001), desc(Detected)) %>%
   tidyr::gather(key=group, value=value, -chnm, -Class) %>%
   mutate(chnm = factor(chnm, levels=unique(chnm)), 
-         group = factor(group, levels=rev(c("AboveEAR0.001", "AboveEAR0.0001", "OnlyDetected"))))
+         group = factor(group, levels=rev(c("AboveEAR0.001", "AboveEAR0.0001", "Detected"))))
 
 chem_detection$value[which(is.na(chem_detection$value))] <- 0
 
 ##########################################
 
 #Make similar table for TQ
-chemicalSummary_bench2 <- chemicalSummary_bench %>%
+
+chem_freq_all<-chemicalSummary_surface %>%
   filter(EAR>0) %>%
+  filter(CAS %in% unique(tox_list_allpocis$chem_data$CAS)) %>%
+  filter(site %in% unique(tox_list_allpocis$chem_site$SiteID)) %>%
+  filter(date >= date_filter[1], date < date_filter[2]) %>%
+  group_by(chnm, site) %>%
+  summarize(EAR_mean = mean(EAR, na.rm=T)) %>%
+  tally(name = "EARDetected")
+
+chemicalSummary_bench2 <- chemicalSummary_bench_surface %>%
+  filter(EAR>0) %>%
+  filter(CAS %in% unique(tox_list_allpocis$chem_data$CAS)) %>%
+  filter(site %in% unique(tox_list_allpocis$chem_site$SiteID)) %>%
+  filter(date >= date_filter[1], date < date_filter[2]) %>%
   dplyr::group_by(site, chnm) %>% 
   summarize(EAR = max(EAR, na.rm=T))
 
@@ -89,25 +106,38 @@ chem_freq_TQ0.01<-chemicalSummary_bench2 %>%
   summarize(EAR_max = max(EAR, na.rm=T)) %>%
   tally(name = "AboveTQ0.01")
 
+chem_freq_TQDetected<-chemicalSummary_bench2 %>%
+  group_by(chnm, site) %>%
+  filter(EAR < 0.01) %>%
+  summarize(EAR_max = max(EAR, na.rm=T)) %>%
+  tally(name = "Detected")
+
 TQ_detection <- full_join(chem_freq_TQ0.1, chem_freq_TQ0.01) %>%
-  full_join(chem_freq_allpocis) %>%
-  left_join(unique(chemicalSummary_allpocis[c("chnm", "Class")])) %>%
+  full_join(chem_freq_TQDetected) %>%
+  full_join(chem_freq_all) %>% 
+  left_join(unique(chemicalSummary_surface[c("chnm", "Class")])) %>%
   rowwise() %>%
-  mutate(EAR_total = sum(AboveTQ0.01, AboveTQ0.1, na.rm=T),
-         OnlyDetected = Detected - EAR_total,
-         chnm = as.character(chnm)) %>%
-  select(-Detected, -EAR_total )
+  mutate(TQ_total = sum(AboveTQ0.01, AboveTQ0.1, Detected, na.rm=T),
+         Diff = EARDetected - TQ_total,
+         chnm = as.character(chnm)) 
+
+TQ_detection$Detected[which(TQ_detection$Diff > 0)] <- TQ_detection$EARDetected[which(TQ_detection$Diff > 0)]
+
+TQ_detection <- TQ_detection %>%
+  select(-EARDetected, -TQ_total, -Diff)
+  
 
 
-TQ_detection$chnm[which(TQ_detection$Class %in% c("Deg - Fungicide", "Deg - Herbicide"))] <- 
-  paste0(TQ_detection$chnm[which(TQ_detection$Class %in% c("Deg - Fungicide", "Deg - Herbicide"))], "*")
+TQ_detection$chnm[which(TQ_detection$Class %in% c("Deg - Fungicide", "Deg - Herbicide", "Deg - Insecticide"))] <- 
+  paste0(TQ_detection$chnm[which(TQ_detection$Class %in% c("Deg - Fungicide", "Deg - Herbicide", "Deg - Insecticide"))], "*")
 TQ_detection$Class <- gsub("Deg - ", "", TQ_detection$Class)
 
 TQ_detection <- TQ_detection %>%
   mutate(Class = factor(Class, c('Herbicide', 'Fungicide', 'Insecticide'))) %>%
   tidyr::gather(key=group, value=value, -chnm, -Class) %>%
   mutate(chnm = factor(chnm, levels=levels(chem_detection$chnm)), 
-         group = factor(group, levels=rev(c("AboveTQ0.1", "AboveTQ0.01", "OnlyDetected"))))
+         group = factor(group, levels=rev(c("AboveTQ0.1", "AboveTQ0.01", "Detected")))) %>%
+  filter (!is.na(chnm))
 
 TQ_detection$value[which(is.na(TQ_detection$value))] <- 0
 
@@ -134,38 +164,40 @@ chemicalbyEAR2 <- ggplot(data=chem_detection, aes(x=chnm, y=value, fill=group)) 
   scale_y_continuous(limits=c(0,15.5), expand=c(0,0)) + 
   theme(legend.position = 'bottom') +
   guides(fill = guide_legend(title.position='left', title.hjust=0.5, reverse=T)) +
-  theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1))
+  theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1)) +
+  ggtitle(paste0("Surface water samples between ", date_filter[1], " and ", date_filter[2]))
 
 print(chemicalbyEAR2)
 
-ggsave(file_out(file.path(path_to_data, "Figures/StackBar_ByEAR_Bychem2.png")), plot = chemicalbyEAR2, height=4, width=6)
+ggsave(file_out(file.path(path_to_data, "Figures/StackBar_ByEAR_Bychem2_watersamples.png")), plot = chemicalbyEAR2, height=4, width=6)
 
 
-
-#Plot barplot by chemical horiztonal
-chemicalbyEAR2_horiztonal <- ggplot(data=chem_detection, aes(x=chnm, y=value, fill=group)) + 
-  geom_bar(color = 'grey', width=.8, size=.1, stat='identity') +  
-  coord_flip() +
-  facet_grid(Class~., space="free", scales="free") +
-  labs(x='Chemical', y='Number of streams', fill = 'EAR') + 
-  theme_bw() +
-  theme(panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank(),
-        panel.background = element_rect(colour = "black", size=.5),
-        axis.title.x=element_blank(),
-        legend.title = element_blank(),
-        axis.text.x = element_text(size=8),
-        axis.text.y = element_text(size=8)) + 
-  scale_fill_brewer(palette = "YlOrRd", labels = c("Detected", expression(paste("EAR > 10"^"-4")), expression(paste("EAR > 10"^"-3")))) +
-  scale_y_continuous(limits=c(0,15.5), expand=c(0,0)) + 
-  theme(legend.position = 'bottom') +
-  guides(fill = guide_legend(title.position='left', title.hjust=0.5, reverse=T)) +
-  theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1))
-
-print(chemicalbyEAR2_horiztonal)
-
-ggsave(file_out(file.path(path_to_data, "Figures/StackBar_ByEAR_Bychem2_horiztonal.png")), plot = chemicalbyEAR2_horiztonal, height=6, width=4)
-
+# 
+# #Plot barplot by chemical horiztonal
+# chemicalbyEAR2_horiztonal <- ggplot(data=chem_detection, aes(x=chnm, y=value, fill=group)) + 
+#   geom_bar(color = 'grey', width=.8, size=.1, stat='identity') +  
+#   coord_flip() +
+#   facet_grid(Class~., space="free", scales="free") +
+#   labs(x='Chemical', y='Number of streams', fill = 'EAR') + 
+#   theme_bw() +
+#   theme(panel.grid.major = element_blank(), 
+#         panel.grid.minor = element_blank(),
+#         panel.background = element_rect(colour = "black", size=.5),
+#         axis.title.x=element_blank(),
+#         legend.title = element_blank(),
+#         axis.text.x = element_text(size=8),
+#         axis.text.y = element_text(size=8)) + 
+#   scale_fill_brewer(palette = "YlOrRd", labels = c("Detected", expression(paste("EAR > 10"^"-4")), expression(paste("EAR > 10"^"-3")))) +
+#   scale_y_continuous(limits=c(0,15.5), expand=c(0,0)) + 
+#   theme(legend.position = 'bottom') +
+#   guides(fill = guide_legend(title.position='left', title.hjust=0.5, reverse=T)) +
+#   theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1)) +
+# ggtitle("Surface Water Samples Jun 1 to Jul 31, 2016")
+# 
+# print(chemicalbyEAR2_horiztonal)
+# 
+# ggsave(file_out(file.path(path_to_data, "Figures/StackBar_ByEAR_Bychem2_horiztonal_surface.png")), plot = chemicalbyEAR2_horiztonal, height=6, width=4)
+# 
 
 
 #Plot barplot by chemical TQ
@@ -186,11 +218,12 @@ TQ_detection2 <- ggplot(data=TQ_detection , aes(x=chnm, y=value, fill=group)) +
   scale_y_continuous(limits=c(0,15.5), expand=c(0,0)) + 
   theme(legend.position = 'bottom') +
   guides(fill = guide_legend(title.position='left', title.hjust=0.5, reverse=T)) +
-  theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1))
+  theme(axis.text.x = element_text(angle=90, vjust=0.5, hjust=1)) +
+  ggtitle(paste0("Surface water samples between ", date_filter[1], " and ", date_filter[2]))
 
 print(TQ_detection2)
 
-ggsave(file_out(file.path(path_to_data, "Figures/StackBar_ByTQ_Bychem2.png")), plot = TQ_detection2, height=4, width=6)
+ggsave(file_out(file.path(path_to_data, "Figures/StackBar_ByTQ_Bychem2_watersamples.png")), plot = TQ_detection2, height=4, width=6)
 
 
 
@@ -200,44 +233,41 @@ ggsave(file_out(file.path(path_to_data, "Figures/StackBar_ByTQ_Bychem2.png")), p
 # Summarize EAR by site
 # #######################
 
-site_freq_allpocis<-chemicalSummary_allpocis %>%
-  filter(EAR > 0) %>%
-  group_by(site, chnm) %>%
-  dplyr::summarize(EAR_mean = mean(EAR, na.rm=T)) %>%
-  tally(name = "Detected")
 
-
-site_freq_EAR0.001<-chemicalSummary2 %>%
+site_freq_EAR0.001<-chemicalSummary2_surface %>%
   group_by(site, chnm) %>%
-  filter(EAR >= 0.001) %>%
+  dplyr::filter(EAR >= 0.001) %>%
   summarize(EAR_max = max(EAR, na.rm=T)) %>%
   tally(name = "AboveEAR0.001")
 
-site_freq_EAR0.0001<-chemicalSummary2 %>%
+site_freq_EAR0.0001<-chemicalSummary2_surface %>%
   group_by(site, chnm) %>%
-  filter(EAR < 0.001 & EAR >= 0.0001) %>%
+  dplyr::filter(EAR < 0.001, EAR >= 0.0001) %>%
   summarize(EAR_max = max(EAR, na.rm=T)) %>%
   tally(name = "AboveEAR0.0001")
 
-site_order <- unique(chemicalSummary$shortName)
+site_freq_EARDetected<-chemicalSummary2_surface %>%
+  group_by(site, chnm) %>%
+  dplyr::filter(EAR > 0 & EAR < 0.0001) %>%
+  summarize(EAR_max = max(EAR, na.rm=T)) %>%
+  tally(name = "EARDetected")
+
+site_order <- unique(chemicalSummary_surface$shortName)
 
 
 site_detection <- full_join(site_freq_EAR0.001, site_freq_EAR0.0001) %>%
-  full_join(site_freq_allpocis) %>%
-  left_join(unique(chemicalSummary[c("site", "shortName", "Lake")])) %>%
+  full_join(site_freq_EARDetected) %>%
+  left_join(unique(chemicalSummary_surface[c("site", "shortName", "Lake")])) %>%
   rowwise() %>%
-  mutate(EAR_total = sum(AboveEAR0.001, AboveEAR0.0001, na.rm=T),
-         OnlyDetected = Detected - EAR_total,
-         Lake = factor(Lake, c("Superior", "Michigan", "Huron", "Erie", "Ontario")),
-         shortName = factor(shortName, site_order)) %>%
-  dplyr::select(-Detected, -EAR_total )
+  mutate(Lake = factor(Lake, c("Superior", "Michigan", "Huron", "Erie", "Ontario")),
+         shortName = factor(shortName, site_order))
 
 
 
 site_detection <- site_detection %>%
-  arrange(Lake, desc(AboveEAR0.001), desc(AboveEAR0.0001), desc(OnlyDetected)) %>%
+  arrange(Lake, desc(AboveEAR0.001), desc(AboveEAR0.0001), desc(EARDetected)) %>%
   tidyr::gather(key=group, value=value, -site, -shortName, -Lake) %>%
-  mutate(group = factor(group, levels=rev(c("AboveEAR0.001", "AboveEAR0.0001", "OnlyDetected"))))
+  mutate(group = factor(group, levels=rev(c("AboveEAR0.001", "AboveEAR0.0001", "EARDetected"))))
 
 site_detection$value[which(is.na(site_detection$value))] <- 0
 
@@ -264,7 +294,7 @@ sitebyEAR2 <- ggplot(data=site_detection, aes(x=shortName, y=value, fill=group))
 
 print(sitebyEAR2)
 
-ggsave(file_out(file.path(path_to_data, "Figures/StackBox_ByEAR_BySite2.png")), plot = sitebyEAR2, height=4, width=6)
+ggsave(file_out(file.path(path_to_data, "Figures/StackBox_ByEAR_BySite2_watersamples.png")), plot = sitebyEAR2, height=4, width=6)
 
 
 
@@ -305,7 +335,7 @@ chemicalSummary3_maxbySite <- chemicalSummary3_maxbySite %>%
   arrange(Class, chnm, desc(EAR))
 
 #summarize and organize TQ
-chemicalSummary_bench3 <- chemicalSummary_bench %>%
+chemicalSummary_bench3 <- chemicalSummary_bench_surface %>%
   mutate(chnm = as.character(chnm))
 chemicalSummary_bench3 $chnm[which(chemicalSummary_bench3$Class %in% c("Deg - Fungicide", "Deg - Herbicide"))] <- 
   paste0(chemicalSummary_bench3 $chnm[which(chemicalSummary_bench3$Class %in% c("Deg - Fungicide", "Deg - Herbicide"))], "*")
