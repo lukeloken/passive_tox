4e93db8eb86c0a3bf11bf8bab1b334f2517a35d0
Luke Loken <lukeloken@gmail.com>
2020-02-10 15:24:20 -0600

    remove site order and move to upstream script

--- Plot_PassiveTox_2016.R
+++ Plot_PassiveTox_2016.R
@@ -219,8 +219,6 @@ site_freq_EAR0.0001<-chemicalSummary2 %>%
   summarize(EAR_max = max(EAR, na.rm=T)) %>%
   tally(name = "AboveEAR0.0001")
 
-site_order <- unique(chemicalSummary$shortName)
-site_ID_order <- unique(chemicalSummary$site)
 
 
 site_detection <- full_join(site_freq_EAR0.001, site_freq_EAR0.0001) %>%
