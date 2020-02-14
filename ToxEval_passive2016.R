020a8c91768ed064544fc2114d9b5e800f1ff58a
Luke Loken <lukeloken@gmail.com>
2020-02-14 11:52:25 -0600

    include ear and tq hits in table

--- ToxEval_passive2016.R
+++ ToxEval_passive2016.R
@@ -32,51 +32,6 @@ site_order <- unique(chemicalSummary$shortName)
 site_ID_order <- unique(chemicalSummary$site)
 
 
-
-#Create csv for publication
-#Chemicals included in analysis
-chemicals_in <- WW_2016_forToxEval %>%
-  group_by(CAS, chnm) %>%
-  select(chnm, CAS, Value) %>%
-  filter(Value > 0) %>%
-  tally(name = 'Detections') %>%
-  full_join(WW_2016_forToxEval[,c('chnm', 'CAS')]) %>%
-  distinct() %>%
-  arrange(chnm) %>%
-  left_join(cas_df[,c('CAS', 'Class')]) %>%
-  filter(Class !="")
-
-chemicals_in$Detections[is.na(chemicals_in$Detections)] <- 0
-
-chemicals_out <- chemicalSummary %>%
-  group_by(Class, chnm, CAS, site) %>%
-  select(Class, chnm, CAS, EAR, site) %>%
-  summarize(EAR = max(EAR, na.rm=T)) %>%
-  filter(EAR > 0) %>%
-  tally(name = 'Detections') %>%
-  full_join(unique(chemicalSummary[,c('chnm', 'CAS', 'Class')])) %>%
-  distinct() %>%
-  arrange(Class, desc(Detections), as.character(chnm))
-
-chemicals_out$Detections[is.na(chemicals_out$Detections)] <- 0
-
-missing_ToxCast <- setdiff(chemicals_in$CAS, chemicals_out$CAS)
-
-chemicals_in[chemicals_in$CAS %in% missing_ToxCast,]
-
-chemicals_combine <- chemicals_in %>%
-  filter(CAS %in% missing_ToxCast) %>%
-  bind_rows(chemicals_out) %>%
-  mutate(Class = factor(Class, c('Herbicide', 'Deg - Herbicide', 'Fungicide', 'Deg - Fungicide', 'Insecticide', 'Deg - Insecticide'))) %>%
-  select(Class, chnm, CAS, Detections) %>%
-  arrange(Class, desc(Detections), chnm) 
-  
-  
-
-write.csv(chemicals_combine, file = file_out(file.path(path_to_data, "Data/Chemicals_characterisitcs.csv")), row.names=F)
-
-write.table(chemicals_combine, file = file_out(file.path(path_to_data, "Data/Chemicals_characterisitcs.txt")), row.names=F, sep=',')
-
 ###############################################################
 # all pocis data
 # These data were above the minimum detection in the POCIS
@@ -121,6 +76,93 @@ chemicalSummary_bench <- tox_bench_list$chem_site %>%
 
 #Add chemical class for chemicals that have new names from toxEval
 chemicalSummary_bench$Class[is.na(chemicalSummary_bench$Class)] <- tox_bench_list$chem_info$Class[match(chemicalSummary_bench$CAS[is.na(chemicalSummary_bench$Class)], tox_bench_list$chem_info$CAS)]
+
+
+
+
+
+
+
+
+
+
+
+#Create csv for publication
+#Chemicals included in analysis
+chemicals_in <- WW_2016_forToxEval %>%
+  group_by(CAS, chnm) %>%
+  select(chnm, CAS, Value) %>%
+  filter(Value > 0) %>%
+  tally(name = 'Detections') %>%
+  full_join(WW_2016_forToxEval[,c('chnm', 'CAS')]) %>%
+  distinct() %>%
+  arrange(chnm) %>%
+  left_join(cas_df[,c('CAS', 'Class')]) %>%
+  filter(Class !="")
+
+# chemicals_in$Detections[is.na(chemicals_in$Detections)] <- 0
+
+chemicals_out <- chemicalSummary %>%
+  group_by(Class, chnm, CAS, site) %>%
+  select(Class, chnm, CAS, EAR, site) %>%
+  summarize(EAR = max(EAR, na.rm=T)) %>%
+  filter(EAR > 0) %>%
+  tally(name = 'Detections') %>%
+  full_join(unique(chemicalSummary[,c('chnm', 'CAS', 'Class')])) %>%
+  distinct() %>%
+  arrange(Class, desc(Detections), as.character(chnm)) %>%
+  filter(Class !="")
+
+# chemicals_out$Detections[is.na(chemicals_out$Detections)] <- 0
+
+missing_ToxCast <- setdiff(chemicals_in$CAS, chemicals_out$CAS)
+
+chemicals_in[chemicals_in$CAS %in% missing_ToxCast,]
+
+chemicals_combine <- chemicals_in %>%
+  filter(CAS %in% missing_ToxCast) %>%
+  bind_rows(chemicals_out) %>%
+  mutate(Class = factor(Class, c('Herbicide', 'Deg - Herbicide', 'Fungicide', 'Deg - Fungicide', 'Insecticide', 'Deg - Insecticide'))) %>%
+  select(Class, chnm, CAS, Detections) %>%
+  arrange(Class, desc(Detections), chnm) 
+
+
+#Find number of EAR 'hits'
+chemicalSummary_EARHits <- chemicalSummary %>%
+  filter(EAR>=.001) %>%
+  dplyr::group_by(chnm, site) %>% 
+  summarize(EAR = max(EAR, na.rm=T)) %>%
+  tally(name = "EAR_Hits") %>%
+  group_by() %>%
+  mutate(chnm = as.character(chnm))
+
+#Find number of TQ 'hits'
+chemicalSummary_TQHits <- chemicalSummary_bench %>%
+  filter(EAR>=.1) %>%
+  dplyr::group_by(chnm, site) %>% 
+  summarize(EAR = max(EAR, na.rm=T)) %>%
+  tally(name = "TQ_Hits") %>%
+  group_by() %>%
+  mutate(chnm = as.character(chnm))
+
+missing_TQ <- setdiff(chemicals_combine$CAS, chemicalSummary_bench$CAS)
+
+chemicals_combine[chemicals_combine$CAS %in% missing_TQ,]
+
+
+chemicals_combine<- full_join(chemicals_combine, chemicalSummary_EARHits) %>%
+  full_join(chemicalSummary_TQHits) %>%
+  arrange(Class, desc(Detections), desc(EAR_Hits), desc(TQ_Hits)) %>%
+  rename('EAR hits' = EAR_Hits,
+         'TQ hits' = TQ_Hits,
+         'Chemical Name' = chnm)
+
+
+write.csv(chemicals_combine, file = file_out(file.path(path_to_data, "Data/Chemicals_characterisitcs.csv")), row.names=F)
+
+write.table(chemicals_combine, file = file_out(file.path(path_to_data, "Data/Chemicals_characterisitcs.txt")), row.names=F, sep=',')
+
+
 
 
 rm(ACClong, ACClong_allpocis, cleaned_ep, filtered_ep )
